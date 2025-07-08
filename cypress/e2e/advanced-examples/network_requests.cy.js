f664da16bd2dd29b260bce96f2aa7360ccab5b69
Tejas <51386360+work-tejas1@users.noreply.github.com>
2025-07-09 00:55:03 +0200

    Project setup and API example
    - Setup project
    - Install prettier dependency
    - Demonstrate API validation test

---
+++ cypress/e2e/advanced-examples/network_requests.cy.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+
+describe('API endpoint status check and Validate user emails', () => {
+  it('check All endpoint responses', () => {
+    const baseUrl = 'https://formatjsononline.com/api';
+    const endpoints = ['/users', '/users/usr_1', '/users/stats'];
+
+    endpoints.forEach(endpoint => {
+      cy.request(baseUrl + endpoint).then(response => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.exist;
+      });
+    });
+  });
+
+  it('check All endpoint responses and Validate user emails', () => {
+    //const baseUrl = 'https://formatjsononline.com/api';
+    const emails = [
+      'john.doe@example.com',
+      'jane.smith@example.com',
+      'mike.johnson@example.com',
+    ];
+
+    cy.request('https://formatjsononline.com/api/users').then(response => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.exist;
+      expect(response.body.data.users[2]).to.have.property(
+        'status',
+        'inactive',
+      );
+      expect(response.body.data.users)
+        .to.be.an('array')
+        .and.have.length(emails.length);
+      response.body.data.users.forEach((user, index) => {
+        expect(user).to.have.property('email', emails[index]);
+        expect(user).to.include.all.keys('email', 'id', 'status');
+      });
+    });
+  });
+});
